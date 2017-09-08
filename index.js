896b9eb89622cb0a2dba2ad7348ddf0e0be54c65
Richard Lim <rlim.richard+flatiron@gmail.com>
2017-09-08 22:10:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ var kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(name){
     kittens.push(name)
+}
 
+function destructivelyPrependKitten(name){
+    kittens.slice(name)
 }
